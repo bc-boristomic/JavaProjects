d55f76911e82bdf4c0b4ed0eade60d905d7b9695
boristomic <boris.tomic@bitcamp.ba>
2015-06-27 13:56:51 +0200

    Completed homework

---
+++ homeworkclasses14/README.md
@@ -0,0 +1,7 @@
+## Dummy X-O
+
+Sto se tice zadace, kao sto smo se dogovorili, cilj vam je da napravite "Dummy TIC-TAC-TOE". On ce se sastojati samo iz tabele 3x3 dugmeta na kojima cete nakon klika naizmjenicno upisivati karaktere (ili stringove) X i O i to na sljedeci nacin:
+- ako ste na dugmetu u 1. redu i 2. koloni, nakon pritiska, upisali X, onda cete na sljedecem dugmetu koje pritisnete, recimo dugme u 2. redu i 3. koloni upisati O.
+- ako se ponovo vratite i pritisnete dugme u 1. redu i 2. koloni, na njemu se treba pojaviti X (tj. ostati ista vrijednost)
+- ako i po treci put pritisnete na dugme u 1. redu i 2. koloni, sada bi trebalo da se upise O.
+- sto se tice ogranicenja, nema nikakvih, nista dodatno ne morate provjeravati (to ce poslije doci samo od sebe :) )
---
+++ homeworkclasses14/src/ba/bitcamp/homeworkclasses14/task1/TicTacToe.java
@@ -0,0 +1,67 @@
+package ba.bitcamp.homeworkclasses14.task1;
+
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class TicTacToe extends JFrame {
+
+	private static final long serialVersionUID = -4734294523692947702L;
+
+	private JButton[] buttons = new JButton [9];
+	private String s = "X";
+
+	/**
+	 * Constructor of TicTacToe Class
+	 */
+	public TicTacToe() {
+		super(); // Call super constructor (Object class)
+		
+		setLayout(new GridLayout(3, 3)); // Layout setup
+
+		for (int i = 0; i < buttons.length; i++) { // Creating and adding buttons, adding listeners to each one
+			buttons[i] = new JButton("");
+			buttons[i].setFont(new Font("Courier New", Font.BOLD, 20));
+			add(buttons[i]);
+			buttons[i].addActionListener(new Listen(buttons[i]));
+		}
+
+		setName("Dummy Tic Tac Toe"); // Name of windows
+		setSize(300, 300); // Size of window
+		setDefaultCloseOperation(EXIT_ON_CLOSE); // Exit on x button
+		setLocationRelativeTo(null); // Set position on center of screen
+		setVisible(true); // Make it visible
+
+	}
+
+	private class Listen implements ActionListener {
+
+		private JButton button;
+		
+		public Listen(JButton button){
+			super();
+			this.button = button;
+		}
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			button.setText(s); // Set button to X
+			
+			if(s.equals("X")){ // If it is X change to O
+				s = "O";
+			} else { // Otherwise change to X
+				s = "X";
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+
+		new TicTacToe(); // Calling the game
+
+	}
+
+}
